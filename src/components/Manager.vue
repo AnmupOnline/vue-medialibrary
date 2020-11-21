52cb6708edb0236f775a41d769ae8ddbef2466a1
Saurav Giri <realanmup@gmail.com>
2020-11-21 18:42:34 +0545

    Update Manager.vue

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -218,7 +218,7 @@
             },
             title: {
                 type: String,
-                required: false
+                required: false,
                 default: 'Media Library'
             }
         },
